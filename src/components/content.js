0995be6a3485413ce3196b5a35823df600b08761
FerSerSan <ferryserranobng04@gmail.com>
2025-10-02 10:39:02 +0200

    asdf

--- src/components/content.js
+++ src/components/content.js
@@ -1,8 +1,13 @@
 export {renderContent}
 
 function renderContent(){
-    const content = document.createElement('div')
-    content.classList.add('content')
-    content.textContent = 'Hola Mundo'
-    return content
+    const tablero = Array(25).fill(0).map((_,i) => 1)
+    return `
+    <div class="container boalrd-Wrapper">
+      <div class="board">
+        ${tablero.map((_,i) => `<div class="board-cell" id="cell-${i}"></div>`).join('')}
+      </div>
+    </div>
+    `;
+
 }
