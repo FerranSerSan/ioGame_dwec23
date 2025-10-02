19628f755bd1bc050cddc3865aa90f4074b6e0ac
FerSerSan <ferryserranobng04@gmail.com>
2025-10-02 10:17:50 +0200

    1

--- src/components/content.js
+++ src/components/content.js
@@ -1,8 +1,14 @@
 export {renderContent}
 
 function renderContent(){
-    const content = document.createElement('div')
-    content.classList.add('content')
-    content.textContent = 'Hola Mundo'
-    return content
+  const fruitsBoard = Array(120).fill(0).map((_,i)=>i)
+    return `
+<div class="container board-wrapper">
+  <div class="board">
+  ${
+    fruitsBoard.map(f=>`<div class="cell">${f}</div>`).join('')
+  }
+  </div>
+</div>
+    `;
 }
